6cd28409f95a837053bd9bf0bcdc680547457f8d
Jennifer Chang <jennifer.chang.bioinform@gmail.com>
2020-10-07 09:23:57 -0500

    Update bd_msgs.R

--- R/bd_msgs.R
+++ R/bd_msgs.R
@@ -4,7 +4,7 @@ data = tibble::tribble(
   "Jennifer","HAPPY BIRTHDAY RICK! Wishing you great fishing weather and error-less condo data transfers. Keep being awesome!",
   "Arun","Happy birthday! I hope you get to eat Jeff's Pizza today and get to install Juicer on all the HPC clusters at ISU.. because that's what you love to do on your bday!",
   "Andrew","Happy birthday! I hope you get to eat Jeff's Pizza today and get to install Juicer on all the HPC clusters at ISU.. because that's what you love to do on your bday!",
-  "Siva","Happy birthday! I hope you get to eat Jeff's Pizza today and get to install Juicer on all the HPC clusters at ISU.. because that's what you love to do on your bday!",
+  "Siva","Happy RAGEday Rick!!",
   "Maryam","Happy birthday Rick! Take a break from the rage channel today!",
   "Satheesh","MANY MORE HAPPY RETURNS OF THE RICK! Hope you have a great day, eat well and get drunk, rock you"
 )
